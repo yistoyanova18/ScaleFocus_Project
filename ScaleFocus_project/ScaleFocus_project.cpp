f405ee96674daade1c8ee8bb0ba8dc32e9f65186
yistoyanova18 <yistoyanova18@codingburgas.bg>
2021-07-08 12:42:21 +0300

    Create getUser() and getAllUsers()

--- ScaleFocus_project/ScaleFocus_project.cpp
+++ ScaleFocus_project/ScaleFocus_project.cpp
@@ -7,20 +7,58 @@
 
 using namespace std;
 
+vector<USER> getUser(nanodbc::connection conn)
+{
+	vector<USER> users;
 
+	nanodbc::statement statement(conn);
+	nanodbc::prepare(statement, NANODBC_TEXT(R"( 
+        SELECT *
+            FROM ScaleFocus_Project.dbo.[User]
+    )"));
+
+	auto result = execute(statement);
+
+	while (result.next())
+	{
+		USER user;
+		user.id = result.get<int>("Id");
+		user.username = result.get<nanodbc::string>("Username", "");
+		user.password = result.get<nanodbc::string>("Password", "");
+		user.firstName = result.get<nanodbc::string>("FirstName", "");
+		user.lastName = result.get<nanodbc::string>("LastName", "");
+		user.dateOfCreation = result.get<nanodbc::string>("DateOfCreation", "");
+		user.idOfCreator = result.get<int>("IdOfCreator", 0);
+		user.dateOfLastChange = result.get<nanodbc::string>("DateLastChange", "");
+		user.idLastChange = result.get<int>("IdLastChange", 0);
+		user.isAdmin = result.get<int>("isAdmin");
+
+		users.push_back(user);
+	}
+
+	return users;
+}
+
+void getAllUsers(nanodbc::connection conn)
+{
+	vector<USER> users = getUser(conn);
+
+	for (size_t i = 0; i < users.size(); i++)
+	{
+		users[i].displayUser();
+	}
+}
 
 int main()
 {
 	try
 	{
-		nanodbc::string connstr = NANODBC_TEXT("DRIVER={ODBC Driver 17 for SQL Server};SERVER=UNIVERSE\\SQLEXPESS;DATABASE=ScaleFocus_Project;Trusted_Connection=yes;"); // an ODBC connection string to your database
+		//nanodbc::string connstr = NANODBC_TEXT("DRIVER={ODBC Driver 17 for SQL Server};Server=UNIVERSE\\SQLEXPESS;DATABASE=ScaleFocus_Project;Trusted_Connection=yes;"); // an ODBC connection string to your database
+		nanodbc::string connstr = NANODBC_TEXT("DRIVER={ODBC Driver 17 for SQL Server};Server=UNIVERSE\\SQLEXPRESS;DATABASE=ScaleFocus_Project;Trusted_Connection=yes;"); // an ODBC connection string to your database
 
 		nanodbc::connection conn(connstr);
 
-		/*do
-		{
-			runProgram(conn);
-		} while (runProgram(conn));*/
+		getAllUsers(conn);
 
 		return EXIT_SUCCESS;
 	}
--- ScaleFocus_project/Structures.h
+++ ScaleFocus_project/Structures.h
@@ -6,39 +6,74 @@
 
 using namespace std;
 
-struct USER 
+struct USER
 {
 	int id = 0;
 	string username = "";
 	string password = "";
 	string firstName = "";
 	string lastName = "";
-	nanodbc::timestamp dateOfCreation;
+	string dateOfCreation;
 	int idOfCreator = 0;
-	nanodbc::timestamp dateOfLastChange;
+	string dateOfLastChange;
 	int idLastChange = 0;
 	bool isAdmin;
+
+	void displayUser()
+	{
+		cout << "User id: " << this->id << endl;
+		cout << "Username: " << this->username << endl;
+		cout << "Password: " << this->password << endl;
+		cout << "First name: " << this->firstName << endl;
+		cout << "Last name: " << this->lastName << endl;
+		cout << "Date of creation: " << this->dateOfCreation << endl;
+		cout << "Id of creator: " << this->idOfCreator << endl;
+		cout << "Date of last change: " << this->dateOfLastChange << endl;
+		cout << "Id of last change: " << this->idLastChange << endl;
+		cout << "Admin: " << this->isAdmin << endl << endl;
+	}
 };
 
 struct TEAM
 {
 	int id = 0;
-	string Title = "";
-	nanodbc::timestamp dateOfCreation;
+	string title = "";
+	string dateOfCreation;
 	int idOfCreator = 0;
-	nanodbc::timestamp dateOfLastChange;
+	string dateOfLastChange;
 	int idLastChange = 0;
+
+	void displayTeam()
+	{
+		cout << "Team id: " << this->id << endl;
+		cout << "Title: " << this->title << endl;
+		cout << "Date of creation: " << this->dateOfCreation << endl;
+		cout << "Id of creator: " << this->idOfCreator << endl;
+		cout << "Date of last change: " << this->dateOfLastChange << endl;
+		cout << "Id of last change: " << this->idLastChange << endl;
+	}
 };
 
 struct PROJECT
 {
 	int id = 0;
-	string Title = "";
-	string Description = "";
-	nanodbc::timestamp dateOfCreation;
+	string title = "";
+	string description = "";
+	string dateOfCreation;
 	int idOfCreator = 0;
-	nanodbc::timestamp dateOfLastChange;
+	string dateOfLastChange;
 	int idLastChange = 0;
+
+	void displayProject()
+	{
+		cout << "Team id: " << this->id << endl;
+		cout << "Title: " << this->title << endl;
+		cout << "Description: " << this->description << endl;
+		cout << "Date of creation: " << this->dateOfCreation << endl;
+		cout << "Id of creator: " << this->idOfCreator << endl;
+		cout << "Date of last change: " << this->dateOfLastChange << endl;
+		cout << "Id of last change: " << this->idLastChange << endl;
+	}
 };
 
 struct TASK
@@ -46,13 +81,26 @@ struct TASK
 	int id = 0;
 	int idOfProject = 0;
 	int idOfAssignee = 0;
-	string Title = "";
-	string Description = "";
-	int status = 0;
-	nanodbc::timestamp dateOfCreation;
+	string title = "";
+	string description = "";
+	enum status { pending, inProgress, complete };
+	string dateOfCreation;
 	int idOfCreator = 0;
-	nanodbc::timestamp dateOfLastChange;
+	string dateOfLastChange;
 	int idLastChange = 0;
+
+	void displayTask()
+	{
+		cout << "Task id: " << this->id << endl;
+		cout << "Project id: " << this->idOfProject << endl;
+		cout << "Assignee id: " << this->idOfAssignee << endl;
+		cout << "Title: " << this->title << endl;
+		cout << "Description: " << this->description << endl;
+		cout << "Date of creation: " << this->dateOfCreation << endl;
+		cout << "Id of creator: " << this->idOfCreator << endl;
+		cout << "Date of last change: " << this->dateOfLastChange << endl;
+		cout << "Id of last change: " << this->idLastChange << endl;
+	}
 };
 
 struct WorkLog
@@ -60,6 +108,15 @@ struct WorkLog
 	int id = 0;
 	int idOfTask = 0;
 	int idOfUser = 0;
-	nanodbc::timestamp time;
-	nanodbc::timestamp date;
+	string time;
+	string date;
+
+	void displayWorkLog()
+	{
+		cout << "Worklog id: " << this->id << endl;
+		cout << "Task id: " << this->idOfTask << endl;
+		cout << "User id: " << this->idOfUser << endl;
+		cout << "Time: " << this->time << endl;
+		cout << "Date: " << this->date << endl;
+	}
 };
