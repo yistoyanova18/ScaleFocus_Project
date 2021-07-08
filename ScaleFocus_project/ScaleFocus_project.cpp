9da0b4474878151951824c0a84695d8c6c5cc474
yistoyanova18 <yistoyanova18@codingburgas.bg>
2021-07-08 17:01:34 +0300

    Create insertUsers() and updateUsers()

--- ScaleFocus_project/ScaleFocus_project.cpp
+++ ScaleFocus_project/ScaleFocus_project.cpp
@@ -13,6 +13,100 @@ void getAllProjects(nanodbc::connection conn);
 void getAllTasks(nanodbc::connection conn);
 void getAllWorkLogs(nanodbc::connection conn);
 
+string enterText()
+{
+	string text;
+	getline(cin, text);
+
+	return text;
+}
+
+int enterInt()
+{
+	string num;
+	getline(cin, num);
+
+	return stoi(num);
+}
+
+void insertUser(nanodbc::connection conn)
+{
+	nanodbc::statement statement(conn);
+	nanodbc::prepare(statement, NANODBC_TEXT(R"(
+        INSERT INTO
+            ScaleFocus_Project.dbo.[User]
+            (Username, Password, FirstName, LastName, DateOfCreation, IdOfCreator, DateLastChange, IdLastChange, isAdmin)
+            VALUES
+            (?, ?, ?, ?, GETDATE(), ?, GETDATE(), ?, 0)
+    )"));
+
+	cout << "Enter the user's username: ";
+	const string Username = enterText();
+	statement.bind(0, Username.c_str());
+
+	cout << "Enter the user's password: ";
+	const string Password = enterText();
+	statement.bind(1, Password.c_str());
+
+	cout << "Enter the user's first name: ";
+	const string firstName = enterText();
+	statement.bind(2, firstName.c_str());
+
+	cout << "Enter the user's last name: ";
+	const string lastName = enterText();
+	statement.bind(3, lastName.c_str());
+
+	cout << "Enter the id of the creator: ";
+	const int idOfCreator = enterInt();
+	statement.bind(4, &idOfCreator);
+
+	cout << "Enter the id of the user that did the last change: ";
+	const int idLastChange = enterInt();
+	statement.bind(5, &idLastChange);
+
+	execute(statement);
+}
+
+void updateUser(nanodbc::connection conn, const int& id)
+{
+	nanodbc::statement statement(conn);
+	nanodbc::prepare(statement, NANODBC_TEXT(R"(
+        UPDATE ScaleFocus_Project.dbo.[User]
+		SET
+            Username = ?
+			,Password = ?
+			,FirstName = ?
+			,LastName = ?
+			,DateLastChange = GETDATE()
+			,IdLastChange = ?
+		WHERE Id = ?;
+    )"));
+
+	cout << "Enter the user's username: ";
+	const string Username = enterText();
+	statement.bind(0, Username.c_str());
+
+	cout << "Enter the user's password: ";
+	const string Password = enterText();
+	statement.bind(1, Password.c_str());
+
+	cout << "Enter the user's first name: ";
+	const string firstName = enterText();
+	statement.bind(2, firstName.c_str());
+
+	cout << "Enter the user's last name: ";
+	const string lastName = enterText();
+	statement.bind(3, lastName.c_str());
+
+	cout << "Enter the id of the user that did the last change: ";
+	const int idLastChange = enterInt();
+	statement.bind(4, &idLastChange);
+
+	statement.bind(5, &id);
+
+	execute(statement);
+}
+
 int main()
 {
 	try
@@ -21,11 +115,17 @@ int main()
 
 		nanodbc::connection conn(connstr);
 
+		int id;
+		cout << "Enter the user's id that you want to change: ";
+		id = enterInt();
+
 		//getAllUsers(conn);
 		//getAllTeams(conn);
 		//getAllProjects(conn);
 		//getAllTasks(conn);
 		//getAllWorkLogs(conn);
+		//insertUser(conn);
+		updateUser(conn, id);
 
 		return EXIT_SUCCESS;
 	}
